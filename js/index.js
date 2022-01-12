20bb9361fae5a4640dff566f5edd99839d7084a5
borjalonsomelero <93580829+borjalonsomelero@users.noreply.github.com>
2022-01-12 15:01:50 +0100

    test

--- js/index.js
+++ js/index.js
@@ -1,22 +1,30 @@
-const navToggle = document.getElementById('navi-toggle');
-const navLinks = document.querySelectorAll('.navigation__link');
+const navToggle = document.getElementById('navi-toggle')
+const navLinks = document.querySelectorAll('.navigation__link')
 
 navLinks.forEach((link) => {
-    link.addEventListener('click', () => {
-        navToggle.checked = false;
-    })
-});
+  link.addEventListener('click', () => {
+    navToggle.checked = false
+  })
+})
 
-new Typed(".typing", {
-    strings: ["enthusiast", "proactive", "smart worker", "ambitious", "passionate", "Borja Alonso"],
-    typeSpeed: 100,
-    backSpeed: 60,
-});
+new Typed('.typing', {
+  strings: [
+    'enthusiast',
+    'proactive',
+    'smart worker',
+    'ambitious',
+    'passionate',
+    'Borja Alonso'
+  ],
+  typeSpeed: 100,
+  backSpeed: 60
+})
 
 function downloadCV(fileUrl, fileName) {
-    let a = document.createElement("a");
-    a.href = fileUrl;
-    a.setAttribute("download", fileName);
-    a.click();
+  let a = document.createElement('a')
+  a.href = fileUrl
+  a.setAttribute('download', fileName)
+  a.click()
 }
 
+console.log('test')
